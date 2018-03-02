a64c64ae963f9c911e5121583ebab0cd202c3dfc
Bertie Blackman <blackmanrgh@gmail.com>
2018-03-02 23:18:12 +0000

    adds axios and makes request to dark-sky endpoint

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 // import styles from './Main.css'
 
@@ -8,6 +9,7 @@ class Main extends React.Component {
 
     this.state = {
       location: null,
+      locationIsLoaded: false,
       errors: []
     }
   }
@@ -24,9 +26,28 @@ class Main extends React.Component {
     })
   }
 
+  getLocationData (coordinates) {
+    const longitude = coordinates[0]
+    const latitude = coordinates[1]
+    const apiQuery = `http://localhost:3000/weather/json?lat=${latitude}&lon=${longitude}`
+
+    return new Promise((resolve, reject) => {
+      axios.get(apiQuery).then((response) => {
+        resolve(response)
+      }).catch((error) => {
+        reject(error)
+      })
+    })
+  }
+
   componentDidMount () {
     this.getLocation().then((res) => {
-      this.setState({location: res})
+      this.setState({location: res, locationIsLoaded: true})
+      this.getLocationData(this.state.location).then((weatherData) => {
+        console.log(weatherData)
+      }).catch((err) => {
+        this.setState({errors: this.state.errors.concat(err)})
+      })
     }).catch((err) => {
       this.setState({errors: this.state.errors.concat(err)})
     })
@@ -36,6 +57,7 @@ class Main extends React.Component {
     return (
       <div>
         <h1>{this.state.location}</h1>
+        {this.state.locationIsLoaded && (<p>I'm loaded</p>)}
       </div>
     )
   }
