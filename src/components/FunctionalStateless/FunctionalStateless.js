8913ace638c39f6a3150379c0331b03e02621f11
bertie.blackman <bertie.blackman@havas.com>
2018-03-06 13:35:58 +0000

    formats with prettier

--- src/components/FunctionalStateless/FunctionalStateless.js
+++ src/components/FunctionalStateless/FunctionalStateless.js
@@ -3,10 +3,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './FunctionalStateless.css'
 
-const FunctionalStateless = ({title}) =>
+const FunctionalStateless = ({ title }) => (
   <div className={styles['main']}>
     <h1 className={styles['main__title']}>{title}</h1>
   </div>
+)
 
 FunctionalStateless.propTypes = {
   title: PropTypes.string.isRequired
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -18,7 +18,7 @@ class Main extends React.Component {
   getLocation () {
     return new Promise((resolve, reject) => {
       try {
-        navigator.geolocation.getCurrentPosition((position) => {
+        navigator.geolocation.getCurrentPosition(position => {
           resolve([position.coords.latitude, position.coords.longitude])
         })
       } catch (err) {
@@ -33,11 +33,14 @@ class Main extends React.Component {
     const apiQuery = `http://localhost:3000/weather/json?lat=${latitude}&lon=${longitude}`
 
     return new Promise((resolve, reject) => {
-      axios.get(apiQuery).then((response) => {
-        resolve(response)
-      }).catch((error) => {
-        reject(error)
-      })
+      axios
+        .get(apiQuery)
+        .then(response => {
+          resolve(response)
+        })
+        .catch(error => {
+          reject(error)
+        })
     })
   }
 
@@ -47,40 +50,51 @@ class Main extends React.Component {
     const apiQuery = `http://localhost:3000/location_data/json?lat=${latitude}&lon=${longitude}`
 
     return new Promise((resolve, reject) => {
-      axios.get(apiQuery).then((response) => {
-        resolve(response)
-      }).catch((error) => {
-        reject(error)
-      })
+      axios
+        .get(apiQuery)
+        .then(response => {
+          resolve(response)
+        })
+        .catch(error => {
+          reject(error)
+        })
     })
   }
 
   componentDidMount () {
-    this.getLocation().then((res) => {
-      this.setState({location: res, locationIsLoaded: true})
+    this.getLocation()
+      .then(res => {
+        this.setState({ location: res, locationIsLoaded: true })
 
-      this.getGooglePlaceImage(this.state.location).then((res) => {
-        console.log(res)
-      }).catch((err) => {
-        this.setState({errors: this.state.errors.concat(err)})
-      })
+        this.getGooglePlaceImage(this.state.location)
+          .then(res => {
+            console.log(res)
+          })
+          .catch(err => {
+            this.setState({ errors: this.state.errors.concat(err) })
+          })
 
-      this.getWeatherData(this.state.location).then((res) => {
-        this.setState({weatherData: res, weatherDataIsLoaded: true})
-      }).catch((err) => {
-        this.setState({errors: this.state.errors.concat(err)})
+        this.getWeatherData(this.state.location)
+          .then(res => {
+            this.setState({ weatherData: res, weatherDataIsLoaded: true })
+          })
+          .catch(err => {
+            this.setState({ errors: this.state.errors.concat(err) })
+          })
       })
-    }).catch((err) => {
-      this.setState({errors: this.state.errors.concat(err)})
-    })
+      .catch(err => {
+        this.setState({ errors: this.state.errors.concat(err) })
+      })
   }
 
   render () {
     return (
       <div>
         <h1>Stuff</h1>
-        {this.state.locationIsLoaded && (<p>{this.state.location}</p>)}
-        {this.state.weatherDataIsLoaded && (<p>{JSON.stringify(this.state.weatherData)}</p>)}
+        {this.state.locationIsLoaded && <p>{this.state.location}</p>}
+        {this.state.weatherDataIsLoaded && (
+          <p>{JSON.stringify(this.state.weatherData)}</p>
+        )}
       </div>
     )
   }
