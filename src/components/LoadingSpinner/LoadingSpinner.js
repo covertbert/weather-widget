fddff35ea5acb2daa1e439c9e287f9a90dfd25a8
Bertie Blackman <blackmanrgh@gmail.com>
2018-03-10 18:53:29 +0000

    adds spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from './LoadingSpinner.pcss'
+
+const FunctionalStateless = props => <div className={styles['spinner']} />
+
+export default FunctionalStateless
