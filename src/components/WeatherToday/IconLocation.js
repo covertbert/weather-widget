97cbeafcaa5fcc6fa89253db39207f9597d19a5c
Bertie Blackman <blackmanrgh@gmail.com>
2018-03-12 21:29:30 +0000

    imports location icon

--- src/components/WeatherToday/IconLocation.js
+++ src/components/WeatherToday/IconLocation.js
@@ -2,12 +2,16 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import ReactAnimatedWeather from 'react-animated-weather'
 
+import locationIcon from '../../assets/img/location-icon.svg'
 import styles from './WeatherToday.pcss'
 
 const IconLocation = ({ iconString, currentLocation }) => (
   <div className={styles['icon-location']}>
     <ReactAnimatedWeather icon={iconString} color={'#fff'} size={60} />
-    <span className={styles['icon-location__location']}>{currentLocation}</span>
+    <div className={styles['icon-location__location']}>
+      <img src={locationIcon} /> >
+      <span>{currentLocation} </span>
+    </div>
   </div>
 )
 
