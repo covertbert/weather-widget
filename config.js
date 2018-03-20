d7448c78a44e6f6a2a256d1df7a1922918f1e64c
bertie.blackman <bertie.blackman@havas.com>
2018-03-20 15:24:21 +0000

    adds env variable setup

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 const config = {
-  API_URL: 'https://dark-sky-endpoint.bertieblackman.co.uk'
+  API_URL:
+    process.env.NODE_ENV === 'development'
+      ? 'https://dark-sky-endpoint.bertieblackman.co.uk'
+      : 'https://dark-sky-endpoint.bertieblackman.co.uk'
 }
 
 export default config
